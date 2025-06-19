40fe732e61c707d8c159f87b12d2b844f25cd4cc
m.baset@zoho.com <m.baset@zoho.com>
2025-06-19 09:38:02 +0300

    updating server port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PROFILE_APP_PORT || 3000;
 
 // Security middleware
 app.use(
